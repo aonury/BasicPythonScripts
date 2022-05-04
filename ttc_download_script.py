f02c8328f786dcf5755eeca758b28ee59267130d
A.Onur YILDIRIM <aonurry@gmail.com>
2022-05-05 00:18:25 +0300

    Update ttc_download_script.py
    First version update!

--- ttc_download_script.py
+++ ttc_download_script.py
@@ -16,9 +16,11 @@ existing_path = ""
 zip_url = "https://eu.tamrieltradecentre.com/download/PriceTable"
 
 if (os.path.exists(ttc_path_document)):
+    print("Your AddOns folder is in your Documents folder! Setting path to: ", ttc_path_document)
     existing_path = ttc_path_document
+
 elif (os.path.exists(ttc_path_onedrive)):
-    print("Seems like you don't have AddOns folder in your Documents Folder. Let's try OneDrive.")
+    print("Your AddOns folder is in your Onedrive folder! Setting path to:", ttc_path_onedrive)
     existing_path = ttc_path_onedrive
 else:
     print("Are you sure you have AddOns folder for Elder Scrolls Online?")
@@ -27,9 +29,14 @@ else:
 # Let's check if we can download and unzip file from url or not:
 if (existing_path != ""):
     http_response = urlopen(zip_url)
+    print("Files are downloaded. Let's extract them")
+    time.sleep(3)
+
     zipfile = ZipFile(BytesIO(http_response.read()))
     zipfile.extractall(path=existing_path)
+    print("Your files are extracted to TTC folder. You can check 'Date Modified' to ensure")
+    time.sleep(3)
 
 else:
-    print("You need to check the problems before running again.")
+    print("You need to solve the problems before running again.")
     time.sleep(3)
